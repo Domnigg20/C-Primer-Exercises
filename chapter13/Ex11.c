d4b71988223476d8f2cd83d2850412b21d71870f
Peter Ariyibi <100387265+Domnigg20@users.noreply.github.com>
2025-08-12 23:04:25 +0100

    Update Ex11.c

--- chapter13/Ex11.c
+++ chapter13/Ex11.c
@@ -1,1 +1,57 @@
 
+/*
+
+
+Write a program that takes two command-line arguments. The first is a string;
+the second is the name of a file. The program should then search the file, printing
+all lines containing the string. Because this task is line oriented rather than
+character oriented, use fgets() instead of getc(). Use the standard C library
+function strstr() (briefly described in exercise 7 of Chapter 11) to search each
+line for the string.
+*/
+
+#include <stdio.h>
+#include<string.h>
+#include <stdlib.h>
+#define LEN 256
+
+
+// 2 CMD LINE 1 string, another name of file
+int main(int argc, char *argv[]) {
+
+char line[LEN];
+
+    if (argc != 3)
+	{
+		printf("Usage: %s <char> [file1] [file2] ...\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	char *search = argv[1];
+    FILE *fp;
+
+ for (int i = 2; i < argc; i++)
+		{
+			
+			if ((fp = fopen(argv[2], "r")) == NULL)
+			{
+				fprintf(stderr, "Can't open file %s\n", argv[2]);
+				continue;
+			}
+
+
+            while (( fgets(line, LEN, fp )) != NULL) {
+                  if (strstr(line, argv[1]) != NULL) {
+                    printf("%s", line);
+        // print line or do something
+
+            }
+
+			fclose(fp);
+		}
+
+
+
+
+}
+}
