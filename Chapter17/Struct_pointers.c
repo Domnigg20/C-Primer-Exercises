02ac79531aa58b206ee276fbce26575bebd600c5
Peter Ariyibi <100387265+Domnigg20@users.noreply.github.com>
2025-11-21 00:02:22 +0000

    Update Struct_pointers.c

--- Chapter17/Struct_pointers.c
+++ Chapter17/Struct_pointers.c
@@ -1,1 +1,35 @@
 
+#include <stdio.h>
+
+
+
+struct gas {
+float distance;
+float gals;
+float MPG;
+};
+
+float gas_dist (struct gas* mpg) {
+
+   printf("Enter car gals: ");
+    scanf("%f", &mpg->MPG);
+
+    printf("Enter distance: ");
+    scanf("%f", &mpg->distance);
+
+    mpg->MPG =(mpg->distance)/ (mpg->MPG);
+
+    return mpg->MPG;
+
+}
+
+int main(void) {
+    struct gas mycar;
+
+    float result =  gas_dist(&mycar);
+    printf("Miles per gallon: %.2f\n", result);
+    return 0;
+
+
+
+}
